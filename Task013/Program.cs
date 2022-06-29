33f2b639b14ddd6fbd4b22e6e0c02e6ac0ee2107
yana sarvarova <serg.yana@mail.ru>
2022-06-30 00:31:55 +0300

    Задача 13

---
+++ Task013/Program.cs
@@ -0,0 +1,16 @@
+﻿/* Программа, которая выводит третью цифру заданного числа или сообщает,
+что третьей цифры не существует */
+
+Console.WriteLine("Введите число");
+string number = Console.ReadLine();
+Zadacha13 (number);
+
+void Zadacha13 (string arg)
+{
+    int length = arg.Length;
+    if (length >= 3)
+    {
+        Console.WriteLine("В числе " + arg + " третья цифра равна " + arg[2]);
+    }
+    else Console.WriteLine("В числе " + arg + " третьей цифры не существует");
+}
