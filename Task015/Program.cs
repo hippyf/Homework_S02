c154c6d5e18a84130c93a00a3af3bec9da347e58
yana sarvarova <serg.yana@mail.ru>
2022-06-30 00:55:00 +0300

    Задача 15

---
+++ Task015/Program.cs
@@ -0,0 +1,13 @@
+﻿/* Программа, которая принимает на вход цифру, обозначающую день недели, 
+и проверяет, является ли этот день выходным */
+
+Console.WriteLine("Введите число от 1 до 7");
+int number = Convert.ToInt32(Console.ReadLine());
+Zadacha15(number);
+
+void Zadacha15 (int arg)
+{
+    if (arg >= 1 && arg <= 5) Console.WriteLine ("НЕТ, не выходной");
+    else if (arg == 6 || arg == 7) Console.WriteLine ("ДА, это выходной!");
+    else Console.WriteLine ("Введено неверное по условиям число");
+}
